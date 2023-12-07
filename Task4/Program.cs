783b030bd76e04e000fdc86ae93898a35b3b13e0
Vladimir <buyanov.vv@yandex.ru>
2023-12-07 23:05:59 +0700

    add 4 task

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+string ReversWordInString(string text) {
+    string newString = string.Empty;
+    for (int i = text.Length-1; i>=0; i--) {
+        string tempWord = string.Empty;
+        if (text[i]==' ' | i==0) {
+            for (int j = i+1; j<text.Length && !(text[j]==' ') ; j++) {
+                tempWord += text[j];
+            }
+            if (i==0) {
+                newString += text[i] + tempWord;
+            } else {
+                newString += tempWord+" ";
+            }
+            
+        }
+    }    
+    return newString;
+}
+
+string text = "Hello my world";
+Console.WriteLine($"'{text}' => '{ReversWordInString(text)}'");
